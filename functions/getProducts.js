f2fd4a1fa1761f8ce872c105aadfe1ae94cad9b9
COv879 <c.olaveherrera@gmail.com>
2024-06-02 12:51:17 -0400

    paquetes añadidos

--- functions/getProducts.js
+++ functions/getProducts.js
@@ -2,13 +2,14 @@
 
 const { Handler } = require('@netlify/functions');
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
 const Product = require('../Backend/models/product');
 const Category = require('../Backend/models/category');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  dialectModule: require('mysql2') 
+  dialectModule: mysql2,
 });
 
 exports.handler = async (event, context) => {
--- functions/searchProducts.js
+++ functions/searchProducts.js
@@ -1,5 +1,6 @@
 const { Handler } = require('@netlify/functions');
 const { Op } = require('sequelize');
+const mysql2 = require('mysql2');
 const Product = require('../Backend/models/product');
 const Category = require('../Backend/models/category');
 const { Sequelize } = require('sequelize');
@@ -8,7 +9,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  dialectModule: require('mysql2')
+  dialectModule: mysql2,
 });
 
 exports.handler = async (event, context) => {
