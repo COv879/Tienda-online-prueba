9c53b10fe87f164e8e114a7ab2f9d70abc46b423
COv879 <c.olaveherrera@gmail.com>
2024-06-02 15:18:08 -0400

    db modificado

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
  
 const Tienda_sequelize = new Sequelize('itnovai_test', 'itnovai_test', 'itnovai_test', {
   host: 'itnovai-test.czny55quvxhk.us-east-1.rds.amazonaws.com',
-  dialect: 'mysql'
+  dialect: 'mysql', 
+  dialectModule: mysql2
 });
 
 module.exports = Tienda_sequelize;
